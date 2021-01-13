063b40b29b794db0cc6ffb643b04cb86d328166b
Nikolay <44638303+IIIIIIIIIIIIIIIIIIIlIIIIIIIIIIIIIIIIIII@users.noreply.github.com>
2021-01-13 12:47:42 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Угадываем от 0 и до.. ?");
         int range = scanner.nextInt();
-        int number = (int) (Math.random() * range);
+        int originalNumber = (int) (Math.random() * range);
         int count = 1;
         System.out.println("Угадайте число от 0 до " + range + ":");
 
         while (true) {
-            int a = scanner.nextInt();
-            if (number > a) {
+            int enteredNumber = scanner.nextInt();
+            if (originalNumber > enteredNumber) {
                 System.out.println("Загаданное число больше");
                 count++;
             }
-            else if (number < a) {
+            else if (originalNumber < enteredNumber) {
                 System.out.println("Загаданное число меньше");
                 count++;
             }
-            else if (number == a) {
+            else if (originalNumber == enteredNumber) {
                 System.out.println("Вы угадали");
                 System.out.println("Количество попыток: " + count);
                 break;
